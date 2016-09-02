8857dbf098991d2ed43cd12fc671e503f54290d9
7sDream <7seconddream@gmail.com>
2016-09-02 14:46:50 +0800

    add css, markdown text

--- main.go
+++ main.go
@@ -2,88 +2,158 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	pathutil "path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/7sDream/rikka/util"
 )
 
-func errHandle(w http.ResponseWriter, err error) {
-	if err != nil {
-		fmt.Println(err.Error())
-		w.Write([]byte(err.Error()))
-		panic(err)
+var password string
+var defaultDomain = "localhost"
+
+const port int = 8000
+
+type viewPhoto struct {
+	Filename string
+	URL      string
+}
+
+func buildURL(r *http.Request) string {
+	host := r.Host
+	if host == "" {
+		host = defaultDomain
+		if port != 80 {
+			host += ":80"
+		}
 	}
+	res := url.URL{
+		Scheme: "http",
+		Host:   host,
+		Path:   "files/" + util.GetFilenameByRequest(r),
+	}
+	return res.String()
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		t, err := template.ParseFiles("templates/index.html")
-		if err == nil {
-			err = t.Execute(w, nil)
-			if err == nil {
-				return
-			}
-		}
-		fmt.Println(err.Error())
-		w.Write([]byte(err.Error()))
-	} else {
-		w.Write([]byte("Error Method."))
+	defer recover()
+
+	if !util.MustBeOr404(w, r, "/") {
+		return
 	}
+
+	if !util.CheckMethod(w, r, "GET") {
+		return
+	}
+
+	util.Render("templates/index.html", w, nil)
 }
 
 func upload(w http.ResponseWriter, r *http.Request) {
-	defer func() {
-		recover()
-	}()
+	defer recover()
 
-	if r.Method == "POST" {
-		err := r.ParseMultipartForm(1024 * 5)
-		errHandle(w, err)
-		file, handle, err := r.FormFile("uploadFile")
-		errHandle(w, err)
-		defer file.Close()
-		saveFile, err := ioutil.TempFile("files", handle.Filename)
-		errHandle(w, err)
-		defer saveFile.Close()
-		io.Copy(saveFile, file)
-		w.Header().Set("Location", "/"+saveFile.Name())
-		w.WriteHeader(302)
-	} else {
-		w.WriteHeader(http.StatusForbidden)
-		w.Write([]byte("Error Method."))
+	if !util.MustBeOr404(w, r, "/upload") {
+		return
 	}
+
+	if !util.CheckMethod(w, r, "POST") {
+		return
+	}
+
+	err := r.ParseMultipartForm(1024 * 5)
+	if util.ErrHandle(w, err) {
+		return
+	}
+
+	userPassword := r.FormValue("password")
+	if userPassword != password {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Error password."))
+		return
+	}
+
+	file, _, err := r.FormFile("uploadFile")
+	if util.ErrHandle(w, err) {
+		return
+	}
+	defer file.Close()
+
+	now := time.Now().UTC()
+	saveFile, err := ioutil.TempFile("files", now.Format("2006-01-02-"))
+	if util.ErrHandle(w, err) {
+		return
+	}
+	defer saveFile.Close()
+
+	io.Copy(saveFile, file)
+
+	_, name := pathutil.Split(saveFile.Name())
+	w.Header().Set("Location", "/view/"+name)
+	w.WriteHeader(302)
 }
 
-func files(w http.ResponseWriter, r *http.Request) {
-	defer func() {
-		recover()
-	}()
+func view(w http.ResponseWriter, r *http.Request) {
+	defer recover()
 
-	if r.Method == "GET" {
-		filepath := r.URL.Path[1:]
-		if _, err := os.Stat(filepath); os.IsNotExist(err) {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-		content, err := ioutil.ReadFile(filepath)
-		errHandle(w, err)
-		w.Write(content)
+	if !util.CheckMethod(w, r, "GET") {
+		return
 	}
+
+	filename := util.GetFilenameByRequest(r)
+	filepath := "files/" + filename
+	if !util.CheckExist(filepath) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("No this photo."))
+	}
+
+	view := viewPhoto{
+		Filename: filename,
+		URL:      buildURL(r),
+	}
+
+	util.Render("templates/view.html", w, view)
 }
 
 func main() {
-	if _, err := os.Stat("files"); os.IsNotExist(err) {
-		os.MkdirAll("files", os.ModeDir)
+	requireFiles := []string{
+		"files",
+		"templates", "templates/index.html", "templates/view.html",
+		"static", "static/main.css", "static/index.css", "static/view.css", "static/rikka.png",
 	}
-	http.HandleFunc("/index", index)
+
+	for _, file := range requireFiles {
+		if !util.CheckExist(file) {
+			fmt.Println(file, "not exist, exit.")
+			return
+		}
+	}
+
+	password = os.Getenv("RIKKA_PWD")
+
+	if password == "" {
+		fmt.Println("No password proivede, use [rikka] as password.")
+		password = "rikka"
+	} else {
+		fmt.Println("Get password from env: ", password)
+	}
+
+	staticFs := http.FileServer(http.Dir("static"))
+	fileFs := http.FileServer(http.Dir("files"))
+
+	http.HandleFunc("/", index)
 	http.HandleFunc("/upload", upload)
-	http.HandleFunc("/files/", files)
+	http.HandleFunc("/view/", view)
+	http.Handle("/files/", http.StripPrefix("/files/", fileFs))
+	http.Handle("/static/", http.StripPrefix("/static/", staticFs))
 
-	fmt.Println("Start rikka...")
+	fmt.Println("Starting rikka...")
 
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe(":"+strconv.Itoa(port), nil)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
---
+++ util/util.go
@@ -0,0 +1,75 @@
+package util
+
+import (
+	"bytes"
+	"html/template"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// ErrHandle is a simple error handl function.
+// If err is an error, write 500 InernalServerError to header and write error message to response and return true.
+// Else (err is nil), don't do anything and return false.
+func ErrHandle(w http.ResponseWriter, err error) bool {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return true
+	}
+	return false
+}
+
+// GetFilenameByRequest gets last part of url path as a filename and return it.
+func GetFilenameByRequest(r *http.Request) string {
+	splitedPath := strings.Split(r.URL.Path, "/")
+	filename := splitedPath[len(splitedPath)-1]
+	return filename
+}
+
+// CheckExist chekc a file or dir is Exist.
+func CheckExist(filepath string) bool {
+	if _, err := os.Stat(filepath); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+// CheckMethod check if request method is as excepted.
+// If not, write the stauts "MethodNotAllow" to header, "Method Not Allowed." to response and return false.
+// Else don't do anything and return true.
+func CheckMethod(w http.ResponseWriter, r *http.Request, excepted string) bool {
+	if r.Method != excepted {
+		http.Error(w, "Method Not Allowed.", http.StatusMethodNotAllowed)
+		return false
+	}
+	return true
+}
+
+// Render is a shortcut function to render template to response.
+func Render(templatePath string, w http.ResponseWriter, data interface{}) {
+	t, err := template.ParseFiles(templatePath)
+	if ErrHandle(w, err) {
+		return
+	}
+
+	buff := bytes.NewBuffer([]byte{})
+
+	err = t.Execute(buff, data)
+	if ErrHandle(w, err) {
+		return
+	}
+	content := make([]byte, buff.Len())
+	buff.Read(content)
+	w.Write(content)
+}
+
+// MustBeOr404 check if URL path is equql to excepted.
+// If not equal, write 404 to header, "404 not fount" to response, and return false.
+// Else don't do anything and return true.
+func MustBeOr404(w http.ResponseWriter, r *http.Request, path string) bool {
+	if r.URL.Path != path {
+		http.NotFound(w, r)
+		return false
+	}
+	return true
+}
