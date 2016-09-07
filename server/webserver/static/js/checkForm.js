cfe6da4e28f0a8242bdbe804e95ceed973a035ed
7sDream <7seconddream@gmail.com>
2016-09-07 11:15:43 +0800

    client side check

--- server/webserver/static/js/checkForm.js
+++ server/webserver/static/js/checkForm.js
@@ -1,12 +1,29 @@
 'use strict';
+
+function isImageType(typeStr) {
+    if (typeStr === "") {
+        return false;
+    }
+    if (typeStr.startsWith("image") === false) {
+        return false;
+    }
+    if (typeStr.includes("vnd")) {
+        return false;
+    }
+    let accepted = ["/jpeg", "/bmp", "/gif", "/png"];
+    return accepted.some((type) => typeStr.includes(type))
+}
+
 function check(maxSizeByMb) {
     let fileInput = document.querySelector("input#uploadFile");
     let file = fileInput.files[0];
-    if (file.type.startsWith("image") === false) {
-        let fileType = file.type || "unknown";
-        alert("Can't upload a " + file.type + " type file");
+    let fileType = file.type
+    if (!isImageType(fileType)) {
+        fileType = fileType || "unknown";
+        alert("Can't upload a " + fileType + " type file");
         return false;
     }
+    console.log("Accept a", fileTypem, "file")
     if (file.size > (maxSizeByMb * 1024 * 1024)) {
         let fileSizeByMb = Math.round(file.size / 1024 / 1024 * 100) / 100;
         alert("Max file size is " + maxSizeByMb + " Mb, input file is " + fileSizeByMb.toString() + " Mb");
