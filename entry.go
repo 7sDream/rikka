21f697606a2be4baca03edc0dc1deee2ea54086b
7sDream <i@7sdre.am>
2019-04-04 04:42:43 +0800

    args log use sublevel, mask password

--- entry.go
+++ entry.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/7sDream/rikka/common/logger"
+	"github.com/7sDream/rikka/common/util"
 	"github.com/7sDream/rikka/plugins"
 	"github.com/7sDream/rikka/server"
 )
@@ -14,14 +15,12 @@ var (
 // Main entry point
 func main() {
 	// print launch args
-	l.Info(
-		"Start rikka with arg:",
-		"bind to socket", socket,
-		", with password", *argPassword,
-		", max file size", *argMaxSizeByMB, "MB",
-		", plugin", *argPluginStr,
-		", log level", *argLogLevel,
-	)
+	l.Info("Start rikka with arg:")
+	l.Info("\t bind to socket", socket)
+	l.Info("\t password", util.MaskString(*argPassword, 3))
+	l.Info("\t max file size", *argMaxSizeByMB, "MB")
+	l.Info("\t plugin", *argPluginStr)
+	l.Info("\t log level", *argLogLevel)
 
 	l.Info("Load plugin...")
 	plugins.Load(thePlugin)
